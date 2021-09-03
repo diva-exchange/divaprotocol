b6a7239560aa4975e9837cc48c8ec78794d4e729
Konrad Baechler <konrad@diva.exchange>
2021-09-03 12:19:32 +0200

    fixed: unknown command error

--- src/protocol/processor.ts
+++ src/protocol/processor.ts
@@ -53,15 +53,13 @@ export class Processor {
         );
         return await this.storeOrderBookOnChain(message);
       case 'contract':
-        //@FIXME
-        return;
-      //return await this.putContract(message);
       case 'subscribe':
+      case 'unsubscribe':
         //@FIXME
         return;
       // return await this.orderBook.getSubscribe(message.channel, message.contract);
       default:
-        throw Error('BusinessProtocol.processOrder(): Invalid Command');
+        throw Error('Processor.process(): Invalid Command');
     }
   }
 
